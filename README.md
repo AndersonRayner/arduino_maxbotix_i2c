a0ce9bffa491f0c7e7235b3ee7437074dede1f6f
matt <anderson_rayner@hotmail.com>
2022-09-19 22:33:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Maxbotix I2C XL Driver for Arduino
+A simple driver for the Maxbotix I2C XL ultrasonic rangefinder via I2C.
+
+The user guide for this driver is available via [Maxbotix](https://www.maxbotix.com/documents/I2CXL-MaxSonar-EZ_Datasheet.pdf)
---
+++ examples/maxbotix_i2c_example/maxbotix_i2c_example.ino
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#include <maxbotix_i2c.h>
+
+MAXBOTIX_I2C rangefinder(Wire1);
+
+
+void setup()
+{
+    Serial.begin(115200);  // Debug serial over USB at 115200 baud
+
+    // Initialize the I2C sensors and ping them
+    rangefinder.init();
+
+}
+
+void loop()
+{
+    // Get readings from all sensors
+    float range;
+    bool success;
+
+    rangefinder.trigger();
+    delay(200);
+    
+    success = rangefinder.read(&range);
+
+    if (success)
+    {
+        Serial.printf("Range: %.2f [ m ]\n",range);
+    }
+    else
+    {
+        Serial.printf("Sensor read unsuccessful\n");
+    }
+    
+    delay(50);
+
+}
